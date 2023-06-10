491cd2362860d256df77d1b52b49a09770e06bd3
maxdesalle <max@sablier.finance>
2023-06-10 18:57:38 +0200

    feat: add support for NFT traits and screenshots

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { wagmiContract } from "./contract";
 import "./App.css";
 import "@rainbow-me/rainbowkit/styles.css";
 
-import { Buffer } from 'buffer';
+import { useRef } from "react";
 
-import { create } from 'ipfs-http-client';
+import { toPng } from "html-to-image";
 
-import { needle } from 'needle';
+import { Buffer } from "buffer";
+
+import { create } from "ipfs-http-client";
+
+import { needle } from "needle";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
@@ -19,11 +23,11 @@ import { privateKeyToAccount } from "viem/accounts";
 import { connect } from "@wagmi/core";
 import { InjectedConnector } from "@wagmi/core/connectors/injected";
 
-
-import { createHash } from 'crypto';
+import { createHash } from "crypto";
 
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import {
 	Address,
 	Hash,
@@ -38,15 +42,27 @@ import {
 import { goerli } from "viem/chains";
 import "viem/window";
 
-
 const client = create({
-  host: 'ipfs.infura.io',
-  port: 5001,
-  protocol: 'https',
-  headers: {
-    authorization: 'Basic ' + Buffer.from('2R0Wd1CUP3U2sONZ9KPtsITQzUL:743776d00d42dff9ccca0b92ae0c77af').toString('base64')
-  }
+	host: "ipfs.infura.io",
+	port: 5001,
+	protocol: "https",
+	headers: {
+		authorization:
+			"Basic " +
+			Buffer.from(
+				"2R0Wd1CUP3U2sONZ9KPtsITQzUL:743776d00d42dff9ccca0b92ae0c77af"
+			).toString("base64"),
+	},
 });
+
+async function downloadImage(url, name) {
+	const link = document.createElement("a");
+	link.href = url;
+	link.download = name;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+}
 
 const publicClient = createPublicClient({
 	chain: goerli,
@@ -60,7 +76,19 @@ const walletClient = createWalletClient({
 
 const token = process.env.REACT_APP_BEARER_TOKEN;
 
+const Tweet = ({ data }) => {
+	// Fill out this component using the structure of a tweet
+	return (
+		<div>
+			<h1>{data.text}</h1>
+			{/* Fill out the rest of the tweet details */}
+		</div>
+	);
+};
+
 function App() {
+	const tweetRef = useRef();
+
 	const [account, setAccount] = useState<Address>();
 	const [hash, setHash] = useState<Hash>();
 	const [receipt, setReceipt] = useState<TransactionReceipt>();
@@ -71,28 +99,117 @@ function App() {
 		const [address] = await walletClient.requestAddresses();
 		setAccount(address);
 	};
+	console.log(token);
 
 	const private_key = process.env.REACT_APP_PRIVATE_KEY;
-
 
 	const mint = async () => {
 		if (!account) return;
 
 		const tweetID = idInput.current!.value as `${number}`;
-		const result = await client.add(`{ "description": "Universal Tweet Object", "image": "https://media.tenor.com/jFn8sS1Et-0AAAAM/cat.gif", "name": ${tweetID}}`);
+
+		const response = await fetch(
+			`https://cors-anywhere.herokuapp.com/https://api.twitter.com/2/tweets/${tweetID}?expansions=attachments.media_keys,author_id&tweet.fields=public_metrics,created_at,entities,geo,possibly_sensitive,source,withheld&media.fields=public_metrics,height,width,url&user.fields=created_at,description,public_metrics`,
+			{
+				method: "GET",
+				mode: "cors",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Access-Control-Allow-Methods": "GET",
+					"Content-Type": "application/json",
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Headers": "Content-Type",
+				},
+			}
+		)
+			.then((response) => response.json())
+			.catch((error) => console.error(error));
+
+		console.log(response);
+		console.log(
+			`https://twitter.com/${response.includes.users[0].username}/status/${tweetID}`
+		);
+
+		const twitterImage = await fetch(
+			"https://cors-anywhere.herokuapp.com/https://tweetpik.com/api/v2/images",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					authorization: "85105ac2-ceb4-446f-8993-53339a5b55fb",
+				},
+				body: JSON.stringify({
+					url: `https://twitter.com/${response.includes.users[0].username}/status/${tweetID}`,
+				}),
+			}
+		).then((response) => response.json());
+
+		console.log(`{ "description": "Universal Tweet Object", "image": "${twitterImage.url}", "name": ${tweetID}, "attributes": [
+    {
+      "trait_type": "Likes", 
+      "value": ${response.data.public_metrics.like_count}
+    },
+    {
+			"display_type": "number",
+      "trait_type": "Bookmarks", 
+      "value": ${response.data.public_metrics.bookmark_count}
+    },
+    {
+			"display_type": "number",
+      "trait_type": "Impressions", 
+      "value": ${response.data.public_metrics.impression_count}
+    },
+    {
+			"display_type": "number",
+      "trait_type": "Quote Retweets", 
+      "value": ${response.data.public_metrics.quote_count}
+    },
+    {
+			"display_type": "number",
+      "trait_type": "Retweets", 
+      "value": ${response.data.public_metrics.retweet_count}
+    },
+    {
+			"display_type": "number",
+      "trait_type": "Comments", 
+      "value": ${response.data.public_metrics.reply_count}
+    }
+  ]}`);
+
+		const result =
+			await client.add(`{ "description": "Universal Tweet Object", "image": "${twitterImage.url}", "name": ${tweetID}, "attributes": [
+    {
+      "trait_type": "Likes", 
+      "value": "${response.data.public_metrics.like_count}"
+    },
+    {
+      "trait_type": "Bookmarks", 
+      "value": "${response.data.public_metrics.bookmark_count}"
+    },
+    {
+      "trait_type": "Impressions", 
+      "value": "${response.data.public_metrics.impression_count}"
+    },
+    {
+      "trait_type": "Quote Retweets", 
+      "value": "${response.data.public_metrics.quote_count}"
+    },
+    {
+      "trait_type": "Retweets", 
+      "value": "${response.data.public_metrics.retweet_count}"
+    },
+    {
+      "trait_type": "Comments", 
+      "value": "${response.data.public_metrics.reply_count}"
+    }
+  ]}`);
 		console.log(result);
 		const ipfsHash = result.path;
 
 		const { request } = await publicClient.simulateContract({
 			...wagmiContract,
 			functionName: "mint",
-			args: [
-				account,
-				tweetID,
-				1,
-				`https://ipfs.io/ipfs/${ipfsHash}`,
-				"0x0",
-			],
+			args: [account, tweetID, 1, `https://ipfs.io/ipfs/${ipfsHash}`, "0x0"],
 			account: privateKeyToAccount(private_key),
 		});
 		const hash = await walletClient.writeContract(request);
@@ -125,5 +242,6 @@ function App() {
 			</>
 		);
 	return <button onClick={connect}>Connect Wallet</button>;
+}
 
 export default App;
